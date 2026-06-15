47e315029a7f8f23f61f7a7b7cc06be7c65d74b4
yanneettop <chesterlp0@gmail.com>
2026-06-15 20:02:05 +0100

    Fix Coffee Matters site name metadata

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,12 +1,12 @@
-const CANONICAL_HOST = 'www.coffeematterslondon.com';
+const CANONICAL_HOST = 'coffeematterslondon.com';
 
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   const forwardedProto = context.request.headers.get('x-forwarded-proto');
   const shouldUseHttps = url.protocol !== 'https:' || forwardedProto === 'http';
-  const shouldUseWww = url.hostname === 'coffeematterslondon.com';
+  const shouldUseBareHost = url.hostname === 'www.coffeematterslondon.com';
 
-  if (shouldUseHttps || shouldUseWww) {
+  if (shouldUseHttps || shouldUseBareHost) {
     url.protocol = 'https:';
     url.hostname = CANONICAL_HOST;
     return Response.redirect(url.toString(), 301);
