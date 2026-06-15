43fd7d2f0fadeea6b342be3345df8d29272ace02
yanneettop <chesterlp0@gmail.com>
2026-06-15 20:11:08 +0100

    Fix canonical redirect loop

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,14 +1,10 @@
-const CANONICAL_HOST = 'coffeematterslondon.com';
-
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   const forwardedProto = context.request.headers.get('x-forwarded-proto');
   const shouldUseHttps = url.protocol !== 'https:' || forwardedProto === 'http';
-  const shouldUseBareHost = url.hostname === 'www.coffeematterslondon.com';
 
-  if (shouldUseHttps || shouldUseBareHost) {
+  if (shouldUseHttps) {
     url.protocol = 'https:';
-    url.hostname = CANONICAL_HOST;
     return Response.redirect(url.toString(), 301);
   }
 
